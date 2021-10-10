2d392cc0934cb1304fa9b3049fb469540dd2098a
Valery Zhuk <zhuk.valeriy.98@gmail.com>
2021-10-10 19:46:57 +0300

    Add light theme, add theme option

---
+++ utils/arguments.py
@@ -0,0 +1,9 @@
+from argparse import ArgumentParser
+
+
+def get_parser() -> ArgumentParser:
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument("path", type=str, help="Addressables build layout file path")
+    parser.add_argument("--theme", type=str, default="dark", choices=["dark", "light"], help="app theme")
+
+    return parser
--- vuabl.py
+++ vuabl.py
@@ -1,21 +1,27 @@
 from dash import Dash, html
 from data.build_layout import BuildLayout
+from argparse import ArgumentParser, Namespace
 import parsing.build_layout as pblt
 import layouts.environment as ltenv
 import layouts.summary as ltsum
 import layouts.group as ltgr
 import layouts.assets as ltasts
+import utils.arguments as arguments
+import utils.theming as theming
 import os
-import sys
 
 
+argumentsParser: ArgumentParser = arguments.get_parser()
+argumentsValues: Namespace = argumentsParser.parse_args()
 
-buildLayoutPath: str = os.path.abspath(sys.argv[1])
-buildLayout: BuildLayout = pblt.read_build_layout(buildLayoutPath)
+theming.theme = argumentsValues.theme
+
+buildLayout: BuildLayout = pblt.read_build_layout(os.path.abspath(argumentsValues.path))
 
 app: Dash = Dash(__name__)
 
 app.layout = html.Div(children=[
+    html.Link(rel="stylesheet", href=theming.get_stylesheet_path()), 
     ltenv.generate_environment_layout(buildLayout.environment), 
     ltsum.generate_summary_layout(buildLayout), 
     ltgr.generate_groups_layout(buildLayout.groups, buildLayout.assetsData), 
