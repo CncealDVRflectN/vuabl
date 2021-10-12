5a94f9e9166bd9c315a59dc547c76fca41764ab7
Valery Zhuk <zhuk.valeriy.98@gmail.com>
2021-10-12 20:39:21 +0300

    Add address and port options

--- utils/arguments.py
+++ utils/arguments.py
@@ -5,6 +5,8 @@ def get_parser() -> ArgumentParser:
     parser: ArgumentParser = ArgumentParser()
     parser.add_argument("path", type=str, help="Addressables build layout file path")
     parser.add_argument("-d", action="store_true", help="enable debug functionality")
+    parser.add_argument("--address", type=str, default="127.0.0.1", help="App address")
+    parser.add_argument("--port", type=int, default=8050, help="App port")
     parser.add_argument("--theme", type=str, default="dark", choices=["dark", "light"], help="app theme")
 
     return parser
