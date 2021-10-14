1e84c3edb1876b0030be38e460b6c92a6c8f2e64
Valery Zhuk <zhuk.valeriy.98@gmail.com>
2021-10-14 20:31:35 +0300

    Adjust command options help messages

--- src/vuabl/utils/arguments.py
+++ src/vuabl/utils/arguments.py
@@ -8,8 +8,8 @@ def get_parser() -> ArgumentParser:
 
     parser.add_argument("path", type=str, help="Addressables build layout file path")
     parser.add_argument("-d", action="store_true", help="enable debug functionality")
-    parser.add_argument("--address", type=str, default="127.0.0.1", help="App address")
-    parser.add_argument("--port", type=int, default=8050, help="App port")
+    parser.add_argument("--address", type=str, default="127.0.0.1", help="app address")
+    parser.add_argument("--port", type=int, default=8050, help="app port")
     parser.add_argument("--theme", type=str, default="dark", choices=["dark", "light"], help="app theme")
 
     return parser
