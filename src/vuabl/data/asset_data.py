a9c4d66fb28cddb9175f60d14d986778bdc5195e
Valery Zhuk <zhuk.valeriy.98@gmail.com>
2021-12-19 16:13:57 +0300

    Improve duplicates table sorting

--- src/vuabl/data/asset_data.py
+++ src/vuabl/data/asset_data.py
@@ -26,3 +26,21 @@ def compare(a: AssetData, b: AssetData) -> int:
         return 1
 
     return 0
+
+
+def compare_with_duplicates(a: AssetData, b: AssetData) -> int:
+    aPathLower: str = a.asset.path.lower()
+    bPathLower: str = b.asset.path.lower()
+    aTotalSize: int = a.size * len(a.referencedByArchives)
+    bTotalSize: int = b.size * len(b.referencedByArchives)
+
+    if aTotalSize < bTotalSize:
+        return 1
+    elif aTotalSize > bTotalSize:
+        return -1
+    elif aPathLower < bPathLower:
+        return -1
+    elif a.asset.path > bPathLower:
+        return 1
+
+    return 0
